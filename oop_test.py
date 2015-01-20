c0ab58fbf1fedf5baf972098c4f9e4e45116d377
taongmakulit2o1 <taongmakulit2o1@gmail.com>
2015-01-20 11:04:52 -0500

    Update oop_test.py

--- oop_test.py
+++ oop_test.py
@@ -6,11 +6,11 @@ WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
-    "class %%%(%%%):":
-      "Make a class named %%% that is-a %%%.",
-    "class %%%(object):\n\tdef __init__(self, ***)" :
-      "class %%% has-a __init__ that takes self and *** parameters.",
-    "class %%%(object):\n\tdef ***(self, @@@)":
+	"class %%%(%%%):":
+	  "Make a class named %%% that is-a %%%.",
+	"class %%%(object):\n\tdef __init__(self, ***)" :
+	  "class %%% has-a __init__ that takes self and *** parameters.",
+	"class %%%(object):\n\tdef ***(self, @@@)":
       "class %%% has-a function named *** that takes self and @@@ parameters.",
     "*** = %%%()":
       "Set *** to an instance of class %%%.",
@@ -20,63 +20,63 @@ PHRASES = {
       "From *** get the *** attribute and set it to '***'."
 }
 
+print PHRASES
+
 # do they want to drill phrases first
 if len(sys.argv) == 2 and sys.argv[1] == "english":
-    PHRASE_FIRST = True
+	PHRASE_FIRST = True
 else:
-    PHRASE_FIRST = False
+	PHRASE_FIRST = False
 
-# load up the words from the website
+# Load up the words from the website
 for word in urlopen(WORD_URL).readlines():
-    WORDS.append(word.strip())
+	WORDS.append(word.strip())
 
 
 def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in
-                   random.sample(WORDS, snippet.count("%%%"))]
-    other_names = random.sample(WORDS, snippet.count("***"))
-    results = []
-    param_names = []
+	class_names = [w.capitalize() for w in
+					random.sample(WORDS, snippet.count("%%%"))]
+	other_names = random.sample(WORDS, snippet.count("***"))
+	results = []
+	param_names = []
 
-    for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1,3)
-        param_names.append(', '.join(random.sample(WORDS, param_count)))
+	for i in range(0, snippet.count("@@@")):
+		param_count = random.randint(1,3)
+		param_names.append(', '.join(random.sample(WORDS, param_count)))
 
-    for sentence in snippet, phrase:
-        result = sentence[:]
+	for sentence in snippet, phrase:
+		result = sentence[:]
 
-        # fake class names
-        for word in class_names:
-            result = result.replace("%%%", word, 1)
+		#fake class names
+		for word in class_names:
+			result = result.replace("%%%", word, 1)
 
-        # fake other names
-        for word in other_names:
-            result = result.replace("***", word, 1)
+		#fake other names
+		for word in other_names:
+			result = result.replace("***", word, 1)
 
-        # fake parameter lists
-        for word in param_names:
-            result = result.replace("@@@", word, 1)
+		# fake parameter lists
+		for word in param_names:
+			result = result.replace("@@@", word, 1)
 
-        results.append(result)
-
-    return results
+		results.append(result)
 
 
 # keep going until they hit CTRL-D
 try:
-    while True:
-        snippets = PHRASES.keys()
-        random.shuffle(snippets)
+	while True:
+		snippets = PHRASES.keys()
+		random.shuffle(snippets)
 
-        for snippet in snippets:
-            phrase = PHRASES[snippet]
-            question, answer = convert(snippet, phrase)
-            if PHRASE_FIRST:
-                question, answer = answer, question
+		for snippet in snippets:
+			phrase = PHRASES[snippet]
+			question, answer = convert(snippet, phrase)
+			if PHRASE_FIRST:
+				question, answer = answer, question
 
-            print question
+			print question
 
-            raw_input("> ")
-            print "ANSWER:  %s\n\n" % answer
+			raw_input("> ")
+			print "ANSWER: %s\n\n" % answer
 except EOFError:
-    print "\nBye"
+	print "\nBye"
